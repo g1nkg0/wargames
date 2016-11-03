686f825edac4653c95cf480d1c637b1027f5f59b
g1nkg0 <g1n.kg0.sec@gmail.com>
2016-11-03 13:33:45 +0100

    g1nkg0 - working on analyze.py

--- OverTheWire/krypton/0304/analyze.py
+++ OverTheWire/krypton/0304/analyze.py
@@ -6,31 +6,33 @@ def unigram(fileIn, fileOut):
     for char in string.uppercase:
         ret.write(char + ' ' + str(fileIn.count(char)) + '\n')
 
-def occ(stringin, sub):
-    count = start = 0
-    while True:
-        start = stringin.find(sub, start) + 1
-        if start > 0:
-            count+=1
-        else:
-            return count
-
-def bigram(fileIn, fileOut):
-    ret = open(fileOut, 'w')
-    for char1 in string.uppercase:
-        for char2 in string.uppercase:
-            ret.write(char1 + char2 + ' ' + str(occ(fileIn, (char1 + char2))) + '\n')
-
 if len(sys.argv) >= 3:
     file1 = open(sys.argv[1], 'r')
     file1 = file1.read()
+    file1.rstrip('\n')
+    file1 = [file1[i:i+6] for i in range(0, len(file1), 6)]
+    file1 = file1[:len(file1)-1]
+    print(file1)
+
     file2 = open(sys.argv[2], 'r')
     file2 = file2.read()
+    file2.rstrip('\n')
+    file2 = [file2[i:i+6] for i in range(0, len(file2), 6)]
+    file2 = file2[:len(file2)-1]
+    print(file2)
 
-    unigram(file1, 'file1.1')
-    unigram(file2, 'file2.1')
+    allnth1 = ''
+    allnth2 = ''
 
-    bigram(file1, 'file1.2')
-    bigram(file2, 'file2.2')
+    for c in range(0, 6):
+        for i in range(len(file1)):
+            allnth1 += file1[i][c]
+            name = 'file1.' + str(c)
+            unigram(allnth1, name)
+
+        for i in range(len(file2)):
+            allnth2 += file2[i][c]
+            name = 'file1.' + str(c)
+            unigram(allnth2, name)
 else:
     print 'Usage: ' + sys.argv[0] + ' /path/to/file1 file2 file3'
